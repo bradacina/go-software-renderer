64ee401f887fd8847ebcb13717f8e1fca7be912c
bradacina <bradacina>
2018-04-04 21:54:09 +1000

    fix matrix multiplication

--- renderer/matrix.go
+++ renderer/matrix.go
@@ -6,9 +6,9 @@ func Mul3x3(a, b, result *Mat3x3) {
 
 func Mul4x4(a, b, result *Mat4x4) {
 	result.AA = a.AA*b.AA + a.AB*b.BA + a.AC*b.CA + a.AD*b.DA
-	result.AB = a.AA*b.BA + a.AB*b.BB + a.AB*b.BC + a.AD*b.BD
-	result.AC = a.AA*b.CA + a.AB*b.CB + a.AB*b.CC + a.AD*b.CD
-	result.AD = a.AA*b.DA + a.AB*b.DB + a.AB*b.DC + a.AD*b.DD
+	result.AB = a.AA*b.BA + a.AB*b.BB + a.AC*b.BC + a.AD*b.BD
+	result.AC = a.AA*b.CA + a.AB*b.CB + a.AC*b.CC + a.AD*b.CD
+	result.AD = a.AA*b.DA + a.AB*b.DB + a.AC*b.DC + a.AD*b.DD
 
 	result.BA = a.BA*b.AA + a.BB*b.BA + a.BC*b.CA + a.BD*b.DA
 	result.BB = a.BA*b.BA + a.BB*b.BB + a.BC*b.BC + a.BD*b.BD
@@ -44,11 +44,4 @@ func Mul4x4WithAfineVertex(a *Mat4x4, b, result *AfineVertex) {
 	result.Y = a.BA*b.X + a.BB*b.Y + a.BC*b.Z + a.BD*b.W
 	result.Z = a.CA*b.X + a.CB*b.Y + a.CC*b.Z + a.CD*b.W
 	result.W = a.DA*b.X + a.DB*b.Y + a.DC*b.Z + a.DD*b.W
-}
-
-func TranslateAfineVertex(a, b, result *AfineVertex) {
-	result.X = a.X + b.X
-	result.Y = a.Y + b.Y
-	result.Z = a.Z + b.Z
-	result.W = 1
 }
